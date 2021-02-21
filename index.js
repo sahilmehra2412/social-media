802aaf84810b3c8ff3f81cde41a6bb197b469463
Sahil Mehra <sahilmehra2412@gmail.com>
2021-02-21 18:28:17 +0530

    Added footer and header

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-    res.render("index",{"name":null});
+    res.render("index");
 })
 
 app.post("/",(req,res)=>{
