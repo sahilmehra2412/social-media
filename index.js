7357d8133b4436fa28b43af16559126a92cbf41b
Sahil Mehra <sahilmehra2412@gmail.com>
2021-02-21 14:03:08 +0530

    First Route

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index",{"name":null});
+})
+
+app.post("/",(req,res)=>{
+    const name = req.body['name'];
+    res.render("index",{"name":name})
+})
+
+app.listen(3000,()=>{
+    console.log("Server Started at 3000");
+})
